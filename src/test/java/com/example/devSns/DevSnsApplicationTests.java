34523cf8087346d68f37c7eda10c900e55392118
sinsehwan <sk124590@gmail.com>
2025-10-04 15:06:19 +0900

    init project setting

---
+++ src/test/java/com/example/devSns/DevSnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devSns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevSnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
